acf4e9e43b62887594c818d341e99734922b5d23
Nikita <knyazkin.nikita@mail.ru>
2020-06-29 05:19:50 +0300

    Создали базу

--- Program.cs
+++ Program.cs
@@ -60,68 +60,6 @@ namespace Test
                 client[i].Print();
             }
 
-            
-            /*
-            for (int i=0;i<N;i++)
-            {
-                Console.WriteLine(str[i]);
-            }
-
-            
-
-            
-            
-            for(int i=0; i < arr.Length; i++)
-            {
-                arr[i]=new list();
-                arr[i].setVal(Int32.Parse(str[i]));
-            }
-// сортировка значений
-            int temp;
-            for (int i = 0; i < arr.Length-1; i++)
-            {
-                for (int j = i + 1; j < arr.Length; j++)
-                {
-                    if (arr[i].getVal() > arr[j].getVal())
-                    {
-                        temp = arr[i].getVal();
-                        arr[i].setVal(arr[j].getVal());
-                        arr[j].setVal(temp);
-                    }
-                }
-            }
-
-// проверка на дубликаты            
-            for (int i=0; i<arr.Length-1; i++)
-            {
-                if (arr[i].getVal()==arr[i+1].getVal())
-                {
-                    if(arr[i].getDupl()==false) arr[i].setDupl(true);
-                    arr[i+1].setDupl(true);
-                }
-            }
-            
-// запись только дубликатов
-            var text = new StreamWriter("output.txt");
-
-            for (int i=0;i<arr.Length;i++)
-            {
-                if (arr[i].getDupl()==false)
-                {
-                    continue;
-                }
-                else
-                {
-                    if (arr[i].getVal()==arr[i+1].getVal())
-                    {
-                        continue;
-                    }
-                    else text.Write(arr[i].getVal());
-                }
-                text.WriteLine();
-            }
-            text.Close();
-            */
         }
     }
 }
